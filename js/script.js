9f440a5199e82446ce3b015a8014006800757d89
DarioLoForti <lofortidario@gmail.com>
2023-12-06 15:44:08 +0100

    snack 2

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 // creo array di bici da corsa con proprietà : nome e peso
 
 let racingBike = [
+
     {
         nome : "Bianchi",
         peso : 1200,
@@ -33,7 +34,7 @@ let bikePesoMinore = racingBike[0];
 
 for (let i = 0; i < racingBike.length; i++) {
     const bikeCorrente = racingBike[i];
-
+    
     if (bikeCorrente.peso < bikePesoMinore.peso) {
         bikePesoMinore = bikeCorrente;
     }
@@ -43,3 +44,64 @@ const { nome, peso } = bikePesoMinore;
 
 // Stampiamo le informazioni sulla bici con il peso minore
 console.log(`La bici più leggera è ${nome} con un peso di ${peso} grammi.`);
+
+
+// SNACK 2
+
+// creo array di oggetti di squadre di calcio.
+
+let teams = [
+
+    {
+        nome : "Milan",
+        punti : 0,
+        falli : 0,
+    },
+
+    {
+        nome : "Inter",
+        punti : 0,
+        falli : 0,
+    },
+
+    {
+        nome : "Juventus",
+        punti : 0,
+        falli : 0,
+    },
+
+    {
+        nome : "Roma",
+        punti : 0,
+        falli : 0,
+    },
+
+    {
+        nome : "Napoli",
+        punti : 0,
+        falli : 0,
+    },
+]
+
+// creo ciclo forEach per inserire valore random le proprietà punti e falli
+
+teams.forEach((elem) => {
+
+    elem.punti = parseInt(Math.floor(Math.random() * 100 + 1 ));
+
+    elem.falli = parseInt(Math.floor(Math.random() * 100 + 1 ));
+})
+
+console.log(teams);
+
+
+// creo nuovo array con soltanto nomi delle squadre e falli subiti
+let teamsFouls = [];
+teams.forEach((elem) => {
+
+    let { nome, falli } = elem;
+    teamsFouls.push(nome, falli);
+
+});
+
+console.log(teamsFouls);
